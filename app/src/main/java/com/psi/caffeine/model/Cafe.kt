b272cb1cd555e78f6663b621ef2e0b3a8e09517f
ACER <loilyxtra@gmail.com>
2022-12-02 19:08:20 +0700

    changed entity to normal model

--- app/src/main/java/com/psi/caffeine/model/Cafe.kt
+++ app/src/main/java/com/psi/caffeine/model/Cafe.kt
@@ -1,28 +1,13 @@
 package com.psi.caffeine.model
 
-import androidx.room.ColumnInfo
-import androidx.room.Entity
-import androidx.room.PrimaryKey
-
-@Entity(tableName = "cafe")
 data class Cafe(
-    @PrimaryKey
-    @ColumnInfo(name = "cafe_id")
     val cafeId: String,
-    @ColumnInfo(name = "cafe_name")
     val cafeName: String,
-    @ColumnInfo(name = "description")
     val description: String,
-    @ColumnInfo(name = "location")
     val location: String,
-    @ColumnInfo(name = "image")
     val image: String,
-    @ColumnInfo(name = "rating")
     val rating: Double,
-    @ColumnInfo(name = "price")
     val distance: Double,
-    @ColumnInfo(name = "open_time")
     val openTime: String,
-    @ColumnInfo(name = "close_time")
     val closeTIme: String
 )
--- app/src/main/java/com/psi/caffeine/model/Menu.kt
+++ app/src/main/java/com/psi/caffeine/model/Menu.kt
@@ -4,19 +4,11 @@ import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "menu")
 data class Menu(
-    @PrimaryKey
-    @ColumnInfo(name = "menu_id")
     val menuId: String,
-    @ColumnInfo(name = "name")
     val name: String,
-    @ColumnInfo(name = "price")
     val price: Int,
-    @ColumnInfo(name = "type")
     val type: String,
-    @ColumnInfo(name = "image")
     val image: String,
-    @ColumnInfo(name = "is_halal")
     val isHalal: Boolean
 )
